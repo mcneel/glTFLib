f46d1ef15a8c55bb6873083e88789590ee39e8aa
Luis Fraguada <luis@mcneel.com>
2018-05-28 12:09:32 +0200

    Cleaned up several codegen efforts and settled on something that I think will work.

--- glTFLibExample/Program.cs
+++ glTFLibExample/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using glTFLib;
 
 namespace glTFLibExample
 {
@@ -10,6 +6,8 @@ namespace glTFLibExample
     {
         static void Main(string[] args)
         {
+            var gltf = new GlTF();
+            //gltf.
         }
     }
 }
